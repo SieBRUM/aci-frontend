a0cec1fef06801b1563719f5ef4821f346ba1855
Angelo Bruggemans <I412999@fhict.nl>
2021-03-01 15:20:56 +0100

    Added material,i18n, localize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ACIRental';
+
+  constructor(
+    public translate: TranslateService,
+    private router: Router
+  ) {
+    this.initLanguage();
+    
+  }
+
+  initLanguage(): void {
+    this.translate.addLangs(['en', 'nl']);
+    this.translate.setDefaultLang('en');
+
+    const localStorageLanguage = localStorage.getItem('language');
+    const browserLang = this.translate.getBrowserLang();
+
+    if (localStorageLanguage && this.translate.langs.includes(localStorageLanguage)) {
+      this.translate.use(localStorageLanguage);
+    } else if (this.translate.langs.includes(browserLang)) {
+      this.translate.use(browserLang);
+    } else {
+      this.translate.use(this.translate.getDefaultLang());
+    }
+
+    localStorage.setItem('language', this.translate.currentLang);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from './material.module';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient);
+}
 
 @NgModule({
   declarations: [
@@ -10,7 +21,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
