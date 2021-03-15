f4185690967bfa49f5f06eea8425106af2233d5b
Marco Ketelaars <i374693@fhict.nl>
2021-03-15 11:57:50 +0100

    Add inventory to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppAddProductPageComponent } from './app-add-product-page/app-add-product-page.component';
 import { AppHomePageComponent } from './app-home-page/app-home-page.component';
+import { InventoryComponent } from './inventory/inventory.component';
 
 /*
   All routing locations of the application.
@@ -10,6 +11,7 @@ import { AppHomePageComponent } from './app-home-page/app-home-page.component';
 const routes: Routes = [
   { path: 'home', component: AppHomePageComponent },
   { path: 'products/add', component: AppAddProductPageComponent },
+  { path: 'products', component: InventoryComponent},
   { path: '**', redirectTo: 'home', pathMatch: 'full' },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppHomePageComponent } from './app-home-page/app-home-page.component';
 import { AppSideMenuBarComponent } from './app-side-menu-bar/app-side-menu-bar.component';
 import { AppAddProductPageComponent } from './app-add-product-page/app-add-product-page.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { LayoutModule } from '@angular/cdk/layout';
 
 export function HttpLoaderFactory(httpClient: HttpClient) {
   return new TranslateHttpLoader(httpClient);
@@ -23,7 +25,8 @@ export function HttpLoaderFactory(httpClient: HttpClient) {
     AppComponent,
     AppHomePageComponent,
     AppSideMenuBarComponent,
-    AppAddProductPageComponent
+    AppAddProductPageComponent,
+    InventoryComponent
   ],
   imports: [
     BrowserModule,
@@ -39,7 +42,8 @@ export function HttpLoaderFactory(httpClient: HttpClient) {
         useFactory: HttpLoaderFactory,
         deps: [HttpClient]
       }
-    })
+    }),
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
