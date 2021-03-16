4984d290ac4ea6e1204c6e2d59a5aa2162254c15
Marco Ketelaars <i374693@fhict.nl>
2021-03-16 10:12:21 +0100

    Change availability boolean to status string

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -3,18 +3,22 @@ import { MatTableModule } from '@angular/material/table';
 import { MatChipsModule } from '@angular/material/chips';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+
+/**
+ * Data die wordt weergegeven in het inventory model
+ */
 export interface ProductData {
   name: string;
   location: string;
-  isAvailable: boolean;
+  status: string;
+  status_class: string;
 }
 
 const TEST_DATA: ProductData[] = [
-  {name: 'CANON R5', location: 'Somewhere', isAvailable: false},
-  {name: 'CANON R5', location: 'Die ene plank', isAvailable: true},
-  {name: 'CANON R5', location: 'Somewhere', isAvailable: false},
-  {name: 'CANON R5', location: 'Somewhere', isAvailable: false},
-  {name: 'CANON R5', location: 'Somewhere', isAvailable: false},
+  {name: 'CANON R5', location: 'Somewhere', status: 'INVENTORY.AWAITS_APPROVAL', status_class: 'awaits-approval-status'},
+  {name: 'CANON R5', location: 'Die ene plank', status: 'INVENTORY.RESERVED', status_class: 'reserved-status'},
+  {name: 'CANON R5', location: 'Somewhere', status: 'INVENTORY.AVAILABLE', status_class: 'available-status'},
+  {name: 'CANON R5', location: 'Somewhere', status: 'INVENTORY.UNAVAILABLE', status_class: 'unavailable-status'},
 ];
 
 @Component({
@@ -23,7 +27,7 @@ const TEST_DATA: ProductData[] = [
   styleUrls: ['./inventory.component.scss']
 })
 export class InventoryComponent {
-    displayedColumns: string[] = ['name', 'location', 'isAvailable'];
+    displayedColumns: string[] = ['name', 'location', 'status'];
     dataSource = TEST_DATA;
 
   constructor(private breakpointObserver: BreakpointObserver) {}
