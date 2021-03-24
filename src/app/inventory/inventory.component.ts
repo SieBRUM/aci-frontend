9bee3b97ce540bc7474034e5b8647347f7cd4cf1
SieBRUM <siebrenkraak@hotmail.com>
2021-03-24 09:29:50 +0100

    Fix cors errors

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductData} from '../models/ProductData';
-import { ProductStatus} from '../models/ProductStatus';
+import { ProductData } from '../models/ProductData';
+import { ProductStatus } from '../models/ProductStatus';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -8,17 +8,17 @@ import { catchError, map, retry } from 'rxjs/operators';
 import { MatTableDataSource } from '@angular/material/table';
 
 const TEST_DATA: ProductData[] = [
-  {name: 'DJ set', location: 'A3.3', status: ProductStatus.AwaitingApproval, reserver: 'Bob Bieb'},
-  {name: 'CANON R5', location: 'Die ene plank', status: ProductStatus.Reserved, reserver: 'Renée François'},
-  {name: 'CANON R5', location: 'Balie', status: ProductStatus.LentOut, reserver: 'John-Paul Sørina'},
-  {name: 'CANON R5', location: 'Kamer A, bovenste plank', status: ProductStatus.Available, reserver: null},
-  {name: 'CANON R5', location: 'A5', status: ProductStatus.Unavailable, reserver: null},
-  {name: 'CANON R5', location: 'Kamer A, bovenste plank', status: ProductStatus.Available, reserver: null},
-  {name: 'CANON R5', location: 'Kamer A, bovenste plank', status: ProductStatus.Available, reserver: null},
-  {name: 'CANON R5', location: 'Kamer A, bovenste plank', status: ProductStatus.Available, reserver: null},
-  {name: 'CANON R5', location: 'Kamer A, bovenste plank', status: ProductStatus.Available, reserver: null},
-  {name: 'CANON R5', location: 'Kamer A, bovenste plank', status: ProductStatus.Available, reserver: null},
-  {name: 'CANON R5', location: 'Kamer A, bovenste plank', status: ProductStatus.Available, reserver: null},
+  { name: 'DJ set', location: 'A3.3', status: ProductStatus.AwaitingApproval, reserver: 'Bob Bieb' },
+  { name: 'CANON R5', location: 'Die ene plank', status: ProductStatus.Reserved, reserver: 'Renée François' },
+  { name: 'CANON R5', location: 'Balie', status: ProductStatus.LentOut, reserver: 'John-Paul Sørina' },
+  { name: 'CANON R5', location: 'Kamer A, bovenste plank', status: ProductStatus.Available, reserver: null },
+  { name: 'CANON R5', location: 'A5', status: ProductStatus.Unavailable, reserver: null },
+  { name: 'CANON R5', location: 'Kamer A, bovenste plank', status: ProductStatus.Available, reserver: null },
+  { name: 'CANON R5', location: 'Kamer A, bovenste plank', status: ProductStatus.Available, reserver: null },
+  { name: 'CANON R5', location: 'Kamer A, bovenste plank', status: ProductStatus.Available, reserver: null },
+  { name: 'CANON R5', location: 'Kamer A, bovenste plank', status: ProductStatus.Available, reserver: null },
+  { name: 'CANON R5', location: 'Kamer A, bovenste plank', status: ProductStatus.Available, reserver: null },
+  { name: 'CANON R5', location: 'Kamer A, bovenste plank', status: ProductStatus.Available, reserver: null },
 ];
 
 @Component({
@@ -27,20 +27,19 @@ const TEST_DATA: ProductData[] = [
   styleUrls: ['./inventory.component.scss']
 })
 export class InventoryComponent implements OnInit {
-    // determined whch columns that are displayed in the inventory table and in which order.
-    displayedColumns: string[] = ['name', 'location', 'reserver', 'status'];
-    dataSource!: MatTableDataSource<ProductData>;
+  // determined whch columns that are displayed in the inventory table and in which order.
+  displayedColumns: string[] = ['name', 'location', 'reserver', 'status'];
+  dataSource!: MatTableDataSource<ProductData>;
 
-    constructor(private breakpointObserver: BreakpointObserver, private http: HttpClient) {}
+  constructor(private breakpointObserver: BreakpointObserver, private http: HttpClient) { }
 
-    ngOnInit(): void {
-      this.getData();
-    }
+  ngOnInit(): void {
+    this.getData();
+  }
 
-    getData(): void {
-      this.http
-            .get<ProductData[]>('http://localhost:44382/Item/inventory/get')
-            .subscribe(data => this.dataSource = new MatTableDataSource(data));
-    }
-
+  getData(): void {
+    this.http
+      .get<ProductData[]>('https://localhost:44372/api/item')
+      .subscribe(data => this.dataSource = new MatTableDataSource(data));
+  }
 }
