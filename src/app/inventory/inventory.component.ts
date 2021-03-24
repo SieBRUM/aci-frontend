07e6caf5346210bed75ea74a77022d8fd413cad4
Marco Ketelaars <i374693@fhict.nl>
2021-03-24 09:20:48 +0100

    Add API call to inventory

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductData} from '../models/ProductData';
 import { ProductStatus} from '../models/ProductStatus';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError, map, retry } from 'rxjs/operators';
+import { MatTableDataSource } from '@angular/material/table';
 
 const TEST_DATA: ProductData[] = [
   {name: 'DJ set', location: 'A3.3', status: ProductStatus.AwaitingApproval, reserver: 'Bob Bieb'},
@@ -22,12 +26,21 @@ const TEST_DATA: ProductData[] = [
   templateUrl: './inventory.component.html',
   styleUrls: ['./inventory.component.scss']
 })
-export class InventoryComponent {
+export class InventoryComponent implements OnInit {
     // determined whch columns that are displayed in the inventory table and in which order.
     displayedColumns: string[] = ['name', 'location', 'reserver', 'status'];
+    dataSource!: MatTableDataSource<ProductData>;
 
-    dataSource = TEST_DATA;
+    constructor(private breakpointObserver: BreakpointObserver, private http: HttpClient) {}
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+    ngOnInit(): void {
+      this.getData();
+    }
+
+    getData(): void {
+      this.http
+            .get<ProductData[]>('http://localhost:44382/Item/inventory/get')
+            .subscribe(data => this.dataSource = new MatTableDataSource(data));
+    }
 
 }
