8cf47fe0c763624b716a36ea99984a8e8bccc837
Mket42 <marco-ketelaars94@hotmail.com>
2021-04-12 13:44:18 +0200

    Add comments to inventory model files

--- src/app/models/ProductData.model.ts
+++ src/app/models/ProductData.model.ts
@@ -4,9 +4,12 @@ import { ProductStatus } from './ProductStatus.enum';
  * Defines data that is used in the inventory table
  */
 export interface ProductData {
+  // The name of the product
   name: string;
+  // The physical location of the product
   location: string;
-  reserver: string | null;
+  // Whether the product requires approval to be rented
   requiresApproval: boolean;
+  // The availability status of the product
   status: ProductStatus;
 }
